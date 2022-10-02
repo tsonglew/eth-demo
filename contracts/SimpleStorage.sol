d223d3dbd2997e2ee6d9361186ceb6f7d492154e
Tsong Lew <tsonglew@gmail.com>
2022-10-02 23:07:49 +0800

    feat: simple storage of types

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+    uint256 favoriteNumber = 5;
+    bool favoriteBool = true;
+    string favoriteString = "String";
+    int256 favoriteInt = -5;
+    address favoriteAddress = 0x9395b1128FD0DC17b2BDA9e1266A61a3A01D4eDe;
+    bytes32 favoriteBytes = "cat"
+}
